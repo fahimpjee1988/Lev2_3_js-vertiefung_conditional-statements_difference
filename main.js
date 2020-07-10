4507bef94a855c6c53dde5996d786d2b4c21fa7c
fahimpjee1988 <adjmal.ra@gmail.com>
2020-07-10 15:00:25 +0200

    fertig

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ function myfunk(){
         document.getElementById("rechnung").innerHTML="not possible"
     }
 }
+
+//Lev2_4_js-vertiefung_conditional-statements_mal5
+
+
+function myfunk2(){
+    if(Number(document.getElementById("input3").value) ==  Number(document.getElementById("input4").value)){
+        document.getElementById("result").innerHTML = ((Number(document.getElementById("input3").value)+Number(document.getElementById("input4").value))*5)
+    } else {
+        document.getElementById("result").innerHTML ="not possible"
+    }
+}
